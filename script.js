2aabb51cc13ad21c3d11697bf48d1650178a76ee
juanTejada <jdtejada000@gmail.com>
2021-04-08 20:18:06 -0500

    actualice-three

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal(".news-cards-1", { delay: 500 });
+ScrollReveal().reveal(".news-cards-2", { delay: 600 });
+ScrollReveal().reveal(".news-cards-3", { delay: 700 });
+ScrollReveal().reveal(".news-cards-4", { delay: 800 });
+ScrollReveal().reveal(".news-cards-5", { delay: 900 });
+ScrollReveal().reveal(".news-cards-6", { delay: 1000 });
+ScrollReveal().reveal(".grid-container-two", { delay: 700 });
