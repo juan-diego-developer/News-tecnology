fdc8887f4e806b3281421beb1cb6c0d436eb3d71
juanTejada <jdtejada000@gmail.com>
2021-04-12 08:48:10 -0500

    actualice-four

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
 });
 
+
 ScrollReveal().reveal('.showcase');
 ScrollReveal().reveal(".news-cards-1", { delay: 500 });
 ScrollReveal().reveal(".news-cards-2", { delay: 600 });
@@ -11,3 +12,4 @@ ScrollReveal().reveal(".news-cards-4", { delay: 800 });
 ScrollReveal().reveal(".news-cards-5", { delay: 900 });
 ScrollReveal().reveal(".news-cards-6", { delay: 1000 });
 ScrollReveal().reveal(".grid-container-two", { delay: 700 });
+
